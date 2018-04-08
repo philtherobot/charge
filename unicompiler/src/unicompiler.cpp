6e7fb42873640109328e910c4025ce07fba5f8a7
Philippe Payant <philtherobot@gmail.com>
2018-04-08 09:41:13 -0400

    Add unicompiler tests

--- unicompiler/src/unicompiler.cpp
+++ unicompiler/src/unicompiler.cpp
@@ -1,5 +1,31 @@
 #include "unicompiler/unicompiler.hpp"
 
-namespace unicompiler{
-    
+namespace unicompiler
+{
+
+YAML::Node configure()
+{
+    YAML::Node conf;
+
+    conf["compiler"] = "/usr/bin/g++";
+    conf["version"] = "7.2.0";
+
+    return conf;
+}   
+
+
+Compiler::Compiler(YAML::Node const & conf)
+{}
+
+
+FileList Compiler::compile(boost::filesystem::path const & source,
+    StringList const & header_paths,
+    StringList const & static_paths,
+    StringList const & system_paths,
+    boost::filesystem::path const & executable_path)
+{
+    return FileList();
+}
+
+
 }
---
+++ unicompiler/test/tests.cpp
@@ -0,0 +1,36 @@
+
+#define BOOST_TEST_MODULE unicompiler
+#include <boost/test/included/unit_test.hpp>
+
+#include "unicompiler/unicompiler.hpp"
+
+BOOST_AUTO_TEST_CASE( configure_gcc )
+{
+    auto conf( unicompiler::configure() );
+
+    BOOST_CHECK_EQUAL( conf["compiler"].as<std::string>(), "/usr/bin/g++");
+}
+
+BOOST_AUTO_TEST_CASE( compile )
+{
+    YAML::Node conf;
+
+    conf["compiler"] = "/usr/bin/g++";
+    conf["version"] = "7.2.0";
+
+    unicompiler::Compiler compiler( conf );
+
+    unicompiler::StringList nothing;
+
+    auto deps =
+        compiler.compile("source.cpp", nothing, nothing, nothing,
+            boost::filesystem::path("/home/user/cache/source"));
+
+    //TODO: from the mock, get...
+    std::string cmd("/usr/bin/g++ source.cpp -o /home/user/cache/source");
+    std::string const expect("/usr/bin/g++ source.cpp -o /home/user/cache/source");
+
+    BOOST_CHECK_EQUAL( cmd, expect );
+
+    BOOST_CHECK( deps.empty() );
+}
