96ead561888be82acf65274d625fec9f64c515cc
Philippe Payant <philippe.payant@microdrones.com>
2020-09-27 14:44:26 -0400

    Functions in test script

---
+++ stirrup/test/integration/process/test.in.ps1
@@ -0,0 +1,37 @@
+Write-Host "Test [Stirrup | SystemProcess]" -ForegroundColor Magenta
+
+function Invoke-ChildProcessActor
+{
+    ${CMAKE_BINARY_DIR}\bin\stirrup_process_invoke ${CMAKE_BINARY_DIR}\bin\stirrup_child_process_actor.exe $args | Out-String
+}
+
+function Test-Equality($value, $expected)
+{
+    if($value -eq $expected)
+    {
+        Write-Host Passed -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "** FAILED" -ForegroundColor DarkRed
+        Write-Host Got `"$value`"
+        Write-Host Expected `"$expected`"
+    }
+}
+
+Write-Host "Case: no argument and zero exit code"
+
+$output = Invoke-ChildProcessActor
+
+$expected = "stirrup_process_invoke: start`r`nstirrup_child_process_actor`r`nstirrup_process_invoke: exit code 0`r`n"
+
+Test-Equality $output $expected
+
+
+Write-Host "Case: two arguments and non-zero exit code"
+
+$output = Invoke-ChildProcessActor arg1 exit_with_error
+
+$expected = "stirrup_process_invoke: start`r`nstirrup_child_process_actor`r`n1: arg1`r`n2: exit_with_error`r`nstirrup_process_invoke: exit code 22`r`n"
+
+Test-Equality $output $expected
